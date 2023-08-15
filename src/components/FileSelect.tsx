e3c3ceb3e904a0a823e04c84a7c0da4d590ee762
Flo2410 <florian@hye.dev>
2023-08-15 22:15:31 +0200

    FileSelect: changed text from "file" to "folder"

--- src/components/FileSelect.tsx
+++ src/components/FileSelect.tsx
@@ -41,8 +41,8 @@ export const FileSelect = () => {
     <div className="flex flex-col">
       <button onClick={handleFileInput}>
         <div className="flex h-10 w-full rounded-md border border-slate-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-300">
-          <span className="px-1">Choose File</span>
-          <span className="px-1">{file_path ?? "No file selected"}</span>
+          <span className="px-1">Choose Folder</span>
+          <span className="px-1">{file_path ?? "No folder selected"}</span>
         </div>
       </button>
     </div>
